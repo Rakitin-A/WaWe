0fc43e0ff0d5e6a6af2a1402028359381521c01a
Rakitin-A <74126183+Rakitin-A@users.noreply.github.com>
2021-09-29 14:46:20 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(function(){
-    var mixer = mixitup('.portfolio__content');
+    
+    $(".menu a, .go-top").on("click", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+    $('.slider-blog__inner').slick({
+        arrows: false,
+        dots:true,
+    });
+
+    $('.menu__btn').on('click',function(){
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+var mixer = mixitup('.portfolio__content');
+
 });
